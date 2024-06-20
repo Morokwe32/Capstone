98e2e722f8ecb5c3154981f274363d153f53fae1
Asteroth72Pillars <219233829@mycput.ac.za>
2024-06-20 13:30:01 +0200

    RoomTypeRepository

--- src/main/java/za/ac/cput/repository/RoomTypeRepository.java
+++ src/main/java/za/ac/cput/repository/RoomTypeRepository.java
@@ -1,16 +1,13 @@
 package za.ac.cput.repository;
 
 import za.ac.cput.domain.RoomType;
-
+import java.util.Optional;
 
 
 public interface RoomTypeRepository
 {
     RoomType create(RoomType roomType);
-
     RoomType findById(Long id);
-
     RoomType update(RoomType roomType);
-
     void delete(RoomType roomType);
 }
