22737b1a29b8630130a3b1ea5815cf47673eae30
Asteroth72Pillars <219233829@mycput.ac.za>
2024-06-20 14:30:33 +0200

    RoomTypeAPI completed

--- src/main/java/za/ac/cput/api/RoomTypeAPI.java
+++ src/main/java/za/ac/cput/api/RoomTypeAPI.java
@@ -1,6 +1,42 @@
 package za.ac.cput.api;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import za.ac.cput.service.RoomTypeService;
+import za.ac.cput.domain.RoomType;
+
+@Component
 public class RoomTypeAPI
 {
+    private final RoomTypeService roomTypeService;
 
+    @Autowired
+    public RoomTypeAPI(RoomTypeService roomTypeService)
+    {
+        this.roomTypeService = roomTypeService;
+    }
+
+    public RoomType create(RoomType roomType)
+    {
+        return roomTypeService.create(roomType);
+    }
+
+    public RoomType read(Long id)
+    {
+        return roomTypeService.read(id);
+    }
+
+    public RoomType update(RoomType roomType)
+    {
+        return roomTypeService.update(roomType);
+    }
+
+    public void delete(Long id)
+    {
+        RoomType roomType = roomTypeService.read(id);
+        if (roomType != null)
+        {
+            roomTypeService.delete(roomType);
+        }
+    }
 }
