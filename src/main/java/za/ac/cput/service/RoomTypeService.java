de16d01c24041f78da5def45235220187157439d
Asteroth72Pillars <219233829@mycput.ac.za>
2024-06-20 13:30:17 +0200

    RoomTypeService

--- src/main/java/za/ac/cput/service/RoomTypeService.java
+++ src/main/java/za/ac/cput/service/RoomTypeService.java
@@ -1,6 +1,49 @@
 package za.ac.cput.service;
 
-public class RoomTypeService
-{
+import org.springframework.stereotype.Service;
+import za.ac.cput.domain.RoomType;
+import za.ac.cput.repository.RoomTypeRepository;
+import java.util.Optional;
+import za.ac.cput.util.Helper;
+import org.springframework.beans.factory.annotation.Autowired;
 
+@Service
+public class RoomTypeService implements IRoomTypeService
+{
+    private final RoomTypeRepository roomTypeRepository;
+
+    @Autowired
+    public RoomTypeService(RoomTypeRepository roomTypeRepository)
+    {
+        this.roomTypeRepository = roomTypeRepository;
+    }
+
+    @Override
+    public RoomType create(RoomType roomType)
+    {
+        return roomTypeRepository.create(roomType);
+    }
+
+    @Override
+    public RoomType read(Long id)
+    {
+        return roomTypeRepository.findById(id);
+    }
+
+    @Override
+    public RoomType update(RoomType roomType)
+    {
+        RoomType existingRoomType = roomTypeRepository.findById(roomType.getTypeId());
+        if (existingRoomType != null)
+        {
+            return roomTypeRepository.update(roomType);
+        }
+        return null;
+    }
+
+    @Override
+    public void delete(RoomType roomType)
+    {
+        roomTypeRepository.delete(roomType);
+    }
 }
